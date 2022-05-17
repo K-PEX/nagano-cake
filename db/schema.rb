3018fbef01b667f7a4ce171e0b84dfa504212a9f
EC2 Default User <tsukasamiyumegu@gmail.com>
2022-05-17 06:38:07 +0000

    deleted delete

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_16_105827) do
+ActiveRecord::Schema.define(version: 2022_05_16_050342) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -40,7 +40,6 @@ ActiveRecord::Schema.define(version: 2022_05_16_105827) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "is_deleted", default: false
     t.index ["email"], name: "index_customers_on_email", unique: true
     t.index ["reset_password_token"], name: "index_customers_on_reset_password_token", unique: true
   end
