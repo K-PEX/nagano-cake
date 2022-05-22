2267ebd720a7dd77c9b6993c4725f3a41a910b83
EC2 Default User <tsukasamiyumegu@gmail.com>
2022-05-22 06:52:46 +0000

    enum-help

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_20_153525) do
+ActiveRecord::Schema.define(version: 2022_05_22_052850) do
 
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 2022_05_20_153525) do
     t.string "delivery_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "customer_id", null: false
   end
 
   create_table "genres", force: :cascade do |t|
